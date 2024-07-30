2218ec535ded14c0429aa8abd9563379a4934c1b
Maria Grozova <marija.grozova@gmail.com>
2024-07-30 11:56:40 +0000

    fix syntax for connecting static files

--- budget_aid/urls.py
+++ budget_aid/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('monthly_budget.urls'), name='home')
+path('admin/', admin.site.urls),
+path('', include('monthly_budget.urls'), name='home'),
 ]
--- monthly_budget/views.py
+++ monthly_budget/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
 # from django.views import generic
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
 class HomePage(TemplateView):
     """
-    Displays home page
+    Displays home page "
     """
     template_name = 'index.html'
