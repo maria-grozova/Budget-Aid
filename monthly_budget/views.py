359d534ab774b510c1ac686ea5d6c68d29812b9c
Maria Grozova <marija.grozova@gmail.com>
2024-07-30 10:26:28 +0000

    create test view

--- monthly_budget/views.py
+++ monthly_budget/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+# from django.views import generic
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomePage(TemplateView):
+    """
+    Displays home page
+    """
+    template_name = 'index.html'
